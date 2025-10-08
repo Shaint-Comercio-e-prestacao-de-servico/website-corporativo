618d25a3e1e45211a18a3fda7f5b18be73247cba
titoosrar <weistertito@gmail.com>
2025-10-08 10:06:44 +0100

    fix: Novas paletas de cores part2

--- src/app/resources/website/shared/loading-page/loading-page.component.ts
+++ src/app/resources/website/shared/loading-page/loading-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-page',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LoadingPageComponent {
 
+  @Input() texto: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
 }
